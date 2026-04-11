e6e95fcd2c11487b5215629f4fefade6e1a757e6
Nanka2411 <ST10496501@rcconnect.edu.za>
2026-04-11 15:24:53 +0200

    Add files via upload

--- AudioPlayer.cs
+++ AudioPlayer.cs
@@ -6,21 +6,28 @@ namespace CyberSecurityChatbot
 {
     public class VoiceGreeting
     {
+        //This method plays a greeting audio file
         public static void PlayGreeting()
         {
             try
             {
+                //Creates the full path to the audio file
+                //It combines the base directory of the app with the "audio" folder and file name
                 string path = Path.Combine(
                     AppDomain.CurrentDomain.BaseDirectory,
                     "Audio",
                     "cyber.wav.wav"
                 );
 
+                //Creates a SoundPlayer object and load the audio file from the path
                 SoundPlayer player = new SoundPlayer(path);
+
+                //plays the audio file
                 player.Play();
             }
             catch (Exception ex)
             {
+                //if a error occurs for example file not found ,it displays an error message.
                 Console.WriteLine("Error playing audio: " + ex.Message);
             }
         }
